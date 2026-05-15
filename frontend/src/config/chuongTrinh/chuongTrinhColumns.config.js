dda4cc9cd29b118f54cde3582480f132487e5f8f
LÊ SƠN <lehoangson11041996@gmail.com>
2026-05-15 12:52:49 +0700

    sua lai be chuan va de hieu

--- frontend/src/config/chuongTrinh/chuongTrinhColumns.config.js
+++
@@ -1,3 +0,0 @@
-import { CHUONG_TRINH_TABLE_CONFIG } from './chuongTrinhTable.config.js';
-
-export const getChuongTrinhColumns = (resourceKey) => CHUONG_TRINH_TABLE_CONFIG[resourceKey]?.columns || [];
